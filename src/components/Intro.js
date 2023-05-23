8bb92a8349137630caa8128516ad0384a5169625
Srishty Mangutte <srishtymangutte@gmail.com>
2023-05-24 04:32:52 +0530

    fix: made responsive

---
+++ src/components/Intro.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/intro.styles.css'
+
+export const Intro = () => {
+  return (
+    <section className='intro-sec'>Intro</section>
+  )
+}
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import { Navbar } from '../components/Navbar';
-
+import { Intro } from '../components/Intro';
+import './style/pages.style.css'
 
 export default function Home() {
   return (
-    <>
+    <div className='page home'>
     <Navbar/>
-    </>
+    <Intro/>
+    </div>
   )
 }
