da49256ab6c324989207e12b818622beeafa2962
Srishty Mangutte <srishtymangutte@gmail.com>
2023-07-04 19:02:08 +0530

    fix: changed onhover styling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home setShowedart={setShowedart} />} />
         <Route index element={<Home setShowedart={setShowedart}/>} />
         <Route path="Article" element={<ShowArticle showing={showarticle} />} />
         <Route path='Dashboard' element={isLoggedIn? <ArticleForm/>:<Home/> }  />
